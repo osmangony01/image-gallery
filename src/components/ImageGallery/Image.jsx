d13bd56e280951b001aa982fa81e126ef70979b6
osmangony01 <mdosmangoni376@gmail.com>
2023-11-03 19:57:18 +0600

    show images in gallery from context and use Resposive grid layout for mobile, tablet and destop divice

--- src/components/ImageGallery/Image.jsx
+++ src/components/ImageGallery/Image.jsx
@@ -1,9 +1,28 @@
+import  { useEffect, useState } from 'react';
 
-const Image = () => {
+const Image = (imgData) => {
+    const [image, setImage] = useState('');
+
+    // get image from the backend
+    const getImage = async (img) => {
+        try {
+            const result = await fetch(`http://localhost:5000/image?img=${img}`) 
+            const data = await result.blob();
+            const url = window.URL.createObjectURL(data)
+            setImage(url)
+        }
+        catch (error) {
+            console.log('error')
+        }
+    }
+
+    useEffect(() => {
+        const { img } = imgData;
+        getImage(img);
+    }, [])
+    
     return (
-        <div>
-            
-        </div>
+        <img src={image} alt='img' className="w-full h-full max-w-full rounded-lg object-contain color:transparent" />
     );
 };
 
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,8 +1,12 @@
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { FaRegImage, } from "react-icons/fa6";
 import AddImages from "../AddImages/AddImages";
+import { ContextAPI } from "../../App";
+import Image from "./Image";
 
 const ImageGallery = () => {
+    const { images, reload, setReload } = useContext(ContextAPI);
+    const [allImage, setAllImage] = useState([]);
 
     const [isOpenAddImageModal, setIsOpenAddImageModal] = useState(false);
 
@@ -11,14 +15,39 @@ const ImageGallery = () => {
         setIsOpenAddImageModal(status)
     }
 
+    useEffect(() => {
+        console.log('render')
+        setAllImage(images);
+    }, [images])
+
     return (
         <div>
-            <div className=' flex justify-center items-center rounded-lg p-4 border-dotted border-2 border-slate-400 flex-col hover:bg-slate-100 cursor-pointer' onClick={() => handleAddImageModal(true)}>
-                <div className=''><FaRegImage></FaRegImage></div>
-                <p className='text-center pt-3'>Add Images</p>
-                {
-                    <AddImages isOpen={isOpenAddImageModal} handleAddImageModal={handleAddImageModal}></AddImages>
-                }
+            <div className="flex justify-between px-6 py-3">
+                <span className="text-xl font-semibold">
+                    <span>Gallery</span>
+                </span>
+            </div>
+            <hr />
+            <div className="p-6">
+                <div className="grid-container">
+                    {
+                        allImage?.map((item, index) => {
+                            return (
+                                <div>
+                                    <Image img={item.image}></Image>
+                                </div>
+                            )
+                        })
+                    }
+
+                    <div className=' flex justify-center items-center rounded-lg p-4 border-dotted border-2 border-slate-400 flex-col hover:bg-slate-100 cursor-pointer' onClick={() => handleAddImageModal(true)}>
+                        <div className=''><FaRegImage></FaRegImage></div>
+                        <p className='text-center pt-3'>Add Images</p>
+                        {
+                            <AddImages isOpen={isOpenAddImageModal} handleAddImageModal={handleAddImageModal}></AddImages>
+                        }
+                    </div>
+                </div>
             </div>
         </div>
     );
