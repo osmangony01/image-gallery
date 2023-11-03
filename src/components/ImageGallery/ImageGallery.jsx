e8e817db7d4d50ded00250c7b6d8bb9c108fa491
osmangony01 <mdosmangoni376@gmail.com>
2023-11-03 22:29:55 +0600

    implement drag-and-drop functionality on images

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,19 +1,34 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { FaRegImage, } from "react-icons/fa6";
 import AddImages from "../AddImages/AddImages";
 import { ContextAPI } from "../../App";
 import Image from "./Image";
 
 const ImageGallery = () => {
+
     const { images, reload, setReload } = useContext(ContextAPI);
     const [allImage, setAllImage] = useState([]);
-
     const [isOpenAddImageModal, setIsOpenAddImageModal] = useState(false);
+    const itemDrag = useRef();
+    const itemDragOver = useRef();
 
     // to handle add-images pop-up modal
     const handleAddImageModal = (status) => {
         setIsOpenAddImageModal(status)
     }
+
+    // sort the images or re-ordering when the user perform drag and drop
+    const handleImageSort = () => {
+        let items = [...allImage];
+        let dragItems = items.splice(itemDrag.current, 1)[0];
+
+        items.splice(itemDragOver.current, 0, dragItems);
+
+        itemDrag.current = null;
+        itemDragOver.current = null;
+
+        setAllImage(items);
+    };
 
     useEffect(() => {
         console.log('render')
@@ -34,7 +49,14 @@ const ImageGallery = () => {
                         allImage?.map((item, index) => {
                             return (
                                 <div
-                                className={`border ${index == 0 && 'card-large'}`}
+                                    key={item._id}
+                                    draggable={true}
+                                    onDragStart={(e) => (itemDrag.current = index)}
+                                    onDragEnter={(e) => (itemDragOver.current = index)}
+                                    onDragEnd={handleImageSort}
+                                    onDragOver={(e) => e.preventDefault()}
+                                    className={`border ${index == 0 && 'card-large'}`}
+                                   
                                 >
                                     <Image img={item.image}></Image>
                                 </div>
